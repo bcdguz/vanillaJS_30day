66e9805523ca6ae4ea86ad1c3f3bc5701c694b43
bcdguz <bcdguz96@gmail.com>
2021-05-06 17:04:24 -0700

    Writing pause/play functions and event listeners

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,3 +6,25 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+// functions
+
+function togglePlay(){
+    if(video.paused) {
+        video.play();
+    } else {
+        video.play();
+    }
+}
+
+function updateButton(){
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon);
+    toggle.textContent = icon;
+}
+// event listeners
+
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
